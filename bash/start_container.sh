0e10026cae81c983c1abd38f5706ba24e723494f
Starchenko Alex <aando.lg@gmail.com>
2018-10-10 09:56:37 +0300

    Add bash scripts for build and create docker container

---
+++ bash/start_container.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -n "$1" ]
+then
+   port=$1
+else
+   port=8090
+fi
+
+echo "Try start container $port"
+docker run --name "aandolg_container_$port" -d -p $port:80 aandolg/nginx:1.0
